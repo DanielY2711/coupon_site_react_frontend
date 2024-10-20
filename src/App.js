1ae94924354269ceb0d795cd55b9e99e78bcd736
DanielY <daniely2711@gmail.com>
2024-10-19 23:35:25 -0600

    changed routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Login';
-import Coupons from './Coupons'
+import Coupons from './Coupons';
 
 const App = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element = {<Navigate to="/login" />} />
+                <Route path="/" element={<Navigate to="/login" />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/coupons" element={<Coupons />} />
                 {/* Add more routes here as needed */}
-                <Route path="/coupons" element={<Coupons/>} />
             </Routes>
         </Router>
     );
